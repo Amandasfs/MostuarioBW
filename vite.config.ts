366856a24b402d174a180272edc8e6d2e99378f3
Amandasfs <amandsfsantos@gmail.com>
2025-09-23 09:50:00 -0300

    atualização e build

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/MostuarioBW/',
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-      },
-    },
-  },
+  base: '/MostuarioBW/', 
 })
